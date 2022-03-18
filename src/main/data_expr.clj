c39de2d9384cacb14305ce3891110b5d32ce34ae
Dregs-2 <49439223+Dregs-2@users.noreply.github.com>
2022-03-18 23:20:29 +0800

    Update data_expr.clj

--- src/main/data_expr.clj
+++ src/main/data_expr.clj
@@ -1,37 +1,39 @@
-(defn- vector-handle [data current]
-  (let [first# (first current) count# (count current)]
+(defn- vector-handle [data expr]
+  (let [first# (first expr) count# (count expr)]
     (condp #(%1 %2) first#
       number?
       (condp = count#
         1 (subvec data first#)
-        2 (subvec data first# (second current))
+        2 (subvec data first# (second expr))
         (throw (IllegalArgumentException. "number type size max is 2")))
       fn?
       (condp = count#
         1 (first# data)
-        2 (first# (second current) data)
-        3 (first# (second current) (nth current 2) data)
-        4 (first# (second current) (nth current 2) (nth current 3) data)
-        5 (first# (second current) (nth current 2) (nth current 3) (nth current 4) data)
-        6 (first# (second current) (nth current 2) (nth current 3) (nth current 4) (nth current 5) data)
+        2 (first# (second expr) data)
+        3 (first# (second expr) (nth expr 2) data)
+        4 (first# (second expr) (nth expr 2) (nth expr 3) data)
+        5 (first# (second expr) (nth expr 2) (nth expr 3) (nth expr 4) data)
+        6 (first# (second expr) (nth expr 2) (nth expr 3) (nth expr 4) (nth expr 5) data)
         (throw (IllegalArgumentException. "fn type maximum number of parameters is 5")))
-      (throw (IllegalArgumentException. (str "unsupported type " (type current)))))
+      (throw (IllegalArgumentException. (str expr))))
     )
   )
+
+
 (defn draw [data expr]
   (if
     (or (nil? data) (empty? expr))
     data
-    (let [current (first expr)]
+    (let [expr# (first expr)]
       (recur
-        (condp #(%1 %2) current
-          number? (nth data current)
-          string? (get data current)
-          symbol? (get data (name current))
-          keyword? (current data)
-          fn? (current data)
-          vector? (vector-handle data current)
-          (throw (IllegalArgumentException. (str "unsupported type " (type current)))))
+        (condp #(%1 %2) expr#
+          number? (nth data expr#)
+          string? (get data expr#)
+          symbol? (get data (name expr#))
+          keyword? (expr# data)
+          fn? (expr# data)
+          vector? (vector-handle data expr#)
+          (throw (IllegalArgumentException. (str expr))))
         (rest expr))
       )
     )
